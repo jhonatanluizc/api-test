54e9b2eb02cbd34d7192a1839a1c28d2ec4c4474
Jhonatan Luiz Chagas <jonatapqt01@gmail.com>
2021-09-28 23:36:22 -0300

    data request

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
-    res.send("Olá, Fábrica de Sinapse e Paulo Salvatore!");
+    var data = new Date();
+    var dia = data.getDate();
+    var mes = data.getMonth();
+    var ano4 = data.getFullYear();
+    var hora = data.getHours();
+    var min = data.getMinutes();
+    var seg = data.getSeconds();
+    var str_data = dia + '/' + (mes + 1) + '/' + ano4;
+    var str_hora = hora + ':' + min + ':' + seg;
+    var dataAtual = ('Hoje é ' + str_data + ' às ' + str_hora);
+    res.send(JSON.stringify(dataAtual));
 });
 
 app.listen(port, () => {
